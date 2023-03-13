134e83e617961cb03d3069bd052fc61b8dd9b647
Liam Hayward <hayli169@student.otago.ac.nz>
2023-03-14 12:50:52 +1300

    Line error test

--- src/cosc202/andie/Andie.java
+++ src/cosc202/andie/Andie.java
@@ -94,8 +94,7 @@ public class Andie {
      * <p>
      * Main entry point to the ANDIE program.
      * </p>
-     * tyler was here 300000
-     * <p>
+     * tyler was here 300000 Liam was also on this line     * <p>
      * Creates and launches the main GUI in a separate thread.
      * As a result, this is essentially a wrapper around {@code createAndShowGUI()}.
      * </p>
