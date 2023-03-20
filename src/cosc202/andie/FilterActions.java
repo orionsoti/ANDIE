e6c036481188214306cae644482174e593c04714
Orion Soti <sotor186@student.otago.ac.nz>
2023-03-20 17:26:26 +1300

    Added GaussianBlurFilter.java and modified FilterActions.java to include the new FilterAction

--- src/cosc202/andie/FilterActions.java
+++ src/cosc202/andie/FilterActions.java
@@ -36,6 +36,7 @@ public class FilterActions {
         actions = new ArrayList<Action>();
         actions.add(new MeanFilterAction("Mean filter", null, "Apply a mean filter", Integer.valueOf(KeyEvent.VK_M)));
         actions.add(new SharpenFilterAction("Sharpen filter", null, "Apply a sharpen filter", Integer.valueOf(KeyEvent.VK_V)));
+        actions.add(new GaussianBlurFilterAction("Gaussian Blur", null, "Apply a guassian filter", Integer.valueOf(KeyEvent.VK_U)));
         actions.add(new MedianFilterAction("Median filter", null, "Apply a median filter", Integer.valueOf(KeyEvent.VK_N)));
     }
 
@@ -223,6 +224,57 @@ public class FilterActions {
             }
             // Create and apply the filter
             target.getImage().apply(new SharpenFilter());
+            target.repaint();
+            target.getParent().revalidate();
+        }
+
+    }
+    public class GaussianBlurFilterAction extends ImageAction {
+
+        /**
+         * <p>
+         * Create a new GaussianBlurFilter action.
+         * </p>
+         * 
+         * @param name     The name of the action (ignored if null).
+         * @param icon     An icon to use to represent the action (ignored if null).
+         * @param desc     A brief description of the action (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
+         */
+        GaussianBlurFilterAction(String name, ImageIcon iconName, String desc, Integer mnemonic) {
+            super(name, iconName, desc, mnemonic);
+        }
+
+        /**
+         * <p>
+         * Callback for when the convert-to-grey action is triggered.
+         * </p>
+         * 
+         * <p>
+         * This method is called whenever the GaussianBlurFilterAction is triggered.
+         * It prompts the user for a filter radius, then applys an appropriately sized
+         * {@link GaussianFilter}.
+         * </p>
+         * 
+         * @param e The event triggering this callback.
+         */
+        public void actionPerformed(ActionEvent e) {
+            // Determine the radius - ask the user.
+            int radius = 1;
+            // Pop-up dialog box to ask for the radius value.
+            SpinnerNumberModel radiusModel = new SpinnerNumberModel(1, 1, 10, 1);
+            JSpinner radiusSpinner = new JSpinner(radiusModel);
+            int option = JOptionPane.showOptionDialog(null, radiusSpinner, "Enter filter radius",
+                    JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+                    null, null);
+            // Returns early if the user cancels the operation.
+            if (option == JOptionPane.CANCEL_OPTION) {
+                return;
+            }else if(option ==JOptionPane.OK_OPTION){
+                radius = radiusModel.getNumber().intValue();
+            }
+            // Create and apply the filter
+            target.getImage().apply(new GaussianBlurFilter(radius));
             target.repaint();
             target.getParent().revalidate();
         }
