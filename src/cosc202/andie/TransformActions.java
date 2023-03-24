9ed9529c26d3c872719c9a1696e4b5c2b3710615
Orion Soti <sotor186@student.otago.ac.nz>
2023-03-24 14:18:10 +1300

    Added resize.java and modified TransformActions.java to include the corresponding method.

--- src/cosc202/andie/TransformActions.java
+++ src/cosc202/andie/TransformActions.java
@@ -23,6 +23,8 @@ public class TransformActions {
         actions = new ArrayList<Action>();
         actions.add(new FlipAction("Flip Vertical", null, "Flip image vertically", null, Flip.FLIP_VERTICAL));
         actions.add(new FlipAction("Flip Horizontal", null, "Flip image horizontally", null, Flip.FLIP_HORIZONTAL));
+        actions.add(new ResizeAction("Resize", null, "Resize image", null));
+        
     }
     /*
      * Create a menu containing the list of Transform actions.
@@ -52,6 +54,67 @@ public class TransformActions {
             target.getImage().apply(new Flip(direction));
             target.repaint();
             target.getParent().revalidate();
+        }
+    }
+
+    public class ResizeAction extends ImageAction{
+        public int height;
+        public int width;
+        public double scale;
+
+        ResizeAction(String name, ImageIcon icon, String desc, Integer mnemonic){
+            super(name, icon, desc, mnemonic);
+        }
+
+        public void actionPerformed(ActionEvent e){
+            height = target.getImage().getCurrentImage().getHeight();
+            width = target.getImage().getCurrentImage().getWidth();
+            scale = 1.0;
+
+            SpinnerNumberModel heightSpinner = new SpinnerNumberModel(height, 0, 10000, 1);
+            SpinnerNumberModel widthSpinner = new SpinnerNumberModel(width, 0, 10000, 1);
+            SpinnerNumberModel scaleSpinner = new SpinnerNumberModel(scale, 0.0, 100.0, 0.1);
+
+            JSpinner h = new JSpinner(heightSpinner);
+            JSpinner w = new JSpinner(widthSpinner);
+            JSpinner s = new JSpinner(scaleSpinner);
+
+            JPanel myPanel = new JPanel();
+            myPanel.add(new JLabel("Height:"));
+            myPanel.add(h);
+            myPanel.add(Box.createHorizontalStrut(15));
+            myPanel.add(new JLabel("Width:"));
+            myPanel.add(w);
+            myPanel.add(Box.createHorizontalStrut(15));
+            myPanel.add(new JLabel("Scale:"));
+            myPanel.add(s);
+
+            int result = JOptionPane.showConfirmDialog(null, myPanel, 
+                       "Resize", JOptionPane.OK_CANCEL_OPTION);
+            if (result == JOptionPane.OK_OPTION) {
+                updateValues((int) h.getValue(), (int) w.getValue(), (double) s.getValue());
+            
+                target.getImage().apply(new Resize(height, width, scale));
+                target.repaint();
+                target.getParent().revalidate();
+            }
+
+        }
+        
+        public void updateValues(int outputHeight, int outputWidth, double outputScale) {
+            if (outputHeight != height) {
+                scale = ((double) outputHeight) / ((double)height);
+                width = (int) (scale * width);
+                height = outputHeight;
+            } else if (outputWidth != width) {
+                scale = ((double) outputWidth) / ((double) width);
+                height = (int) (scale * height);
+                width = outputWidth;
+            } else if (outputScale != scale) {
+                scale = outputScale;
+                width = (int) (scale * width);
+                height = (int) (scale * height);
+            }
         }
     }
 }
