69fc22813243bc2dea083e6ca927410a840aca00
myrja100 <myrja100@gmail.com>
2023-03-24 14:25:14 +1300

    Fixed the zoomFull ViewAction not repainting

--- src/cosc202/andie/ViewActions.java
+++ src/cosc202/andie/ViewActions.java
@@ -197,7 +197,7 @@ public class ViewActions {
          */
         public void actionPerformed(ActionEvent e) {
             target.setZoom(100);
-            target.revalidate();
+            target.repaint();
             target.getParent().revalidate();
         }
 
